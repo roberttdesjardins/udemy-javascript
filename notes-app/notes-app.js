c35fe571f2056245bad05b22e4a55b672b937bf0
Robert Desjardins <roberttdesjardins@gmail.com>
2018-07-18 11:51:37 -0700

    part 59 completed

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -11,8 +11,35 @@ const notes = [{
     body: "Get a new seat"
 }]
 
+const filters = {
+    searchText: ""
+}
 
-// Add a new element
-const newParagraph = document.createElement("p")
-newParagraph.textContent = "New Paragraph"
-document.body.appendChild(newParagraph)
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    document.querySelector("#notes-div").innerHTML = ""
+    filteredNotes.forEach(function(note) {
+        const noteElement = document.createElement("p")
+        noteElement.textContent = note.title
+        document.querySelector("#notes-div").appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector("#create-note").addEventListener("click", function(e) {
+    console.log("Click worked")
+    e.target.textContent = "stop clicking me"
+})
+
+
+document.querySelector("#search-text").addEventListener("input", function(e){
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector("#for-fun-check").addEventListener("change", function(e) {
+    console.log(e.target.checked)
+})
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,18 +15,63 @@ const todo = [{
     completed: true
 }]
 
-// Print summary method: You have 2 todos left (p element)
-// Add a p for each todo above (user text value)
-const incompleteTodos = todo.filter(function(todo){
-    return !todo.completed
-})
-const summary = document.createElement("h5")
-summary.textContent = `You have ${incompleteTodos.length} todo(s) left`
-document.body.appendChild(summary)
 
-todo.forEach(function(todo) {
-    const p = document.createElement("p")
-    p.textContent = todo.text
-    document.body.appendChild(p)
+const filteredTodo = {
+    searchField: "",
+    hideCompleted: false
+}
+
+const filterTodo = function(array, searchFilter) {
+    return array.filter(function(element) {
+        const searchTextMatch = element.text.toLowerCase().includes(searchFilter.searchField.toLowerCase())
+        const hideCompletedMatch = !searchFilter.hideCompleted || !element.completed
+        return searchTextMatch && hideCompletedMatch
+    })
+}
+
+const renderTodos = function(array, filter) {
+    let filteredArray = filterTodo(array, filter)
+
+
+    document.querySelector("#todo-uncompleted-div").innerHTML = ""
+    document.querySelector("#todo-div").innerHTML = ""
+
+    const incompleteTodos = filteredArray.filter(function(todo){
+        return !todo.completed
+    })
+
+    const summary = document.createElement("h5")
+    summary.textContent = `You have ${incompleteTodos.length} todo(s) left`
+    document.querySelector("#todo-uncompleted-div").appendChild(summary)
+
+
+    filteredArray.forEach(function(element) {
+        let todoParagraph = document.createElement("p")
+        todoParagraph.textContent = element.text
+        document.querySelector("#todo-div").appendChild(todoParagraph)
+    })
+}
+
+renderTodos(todo, filteredTodo)
+
+// Listen for filter TODOs text change
+document.querySelector("#filter-input").addEventListener("input", function(e){
+    filteredTodo.searchField = e.target.value
+    renderTodos(todo, filteredTodo)
 })
 
+// Listen for create TODO submission
+document.querySelector("#add-todo-form").addEventListener("submit", function(e){
+    e.preventDefault()
+    todo.push({
+        text: e.target.elements.todoTitle.value,
+        completed: false
+    })
+    e.target.elements.todoTitle.value = ""
+    renderTodos(todo, filteredTodo)
+})
+
+document.querySelector("#hide-completed-check").addEventListener("change", function(e) {
+    filteredTodo.hideCompleted = e.target.checked
+    renderTodos(todo, filteredTodo)
+})
